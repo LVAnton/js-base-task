93cba44861f19b254ed47c6cf04ea240ca75ad55
Vikentii Ekgart <vikegart@gmail.com>
2021-10-08 19:18:01 +0400

    erased solution

--- src/transform-first-letter-to-uppercase.js
+++ src/transform-first-letter-to-uppercase.js
@@ -1,12 +1,5 @@
 const transformFirstLetterToUppercase = (str) => {
-  if (typeof str != "string")
-    return "Passed argument is not a string";
-  const words = str.split(" ");
-  const capitalizedWords = words.map(word => {
-    const [firstLetter, ...others] = word;
-    return firstLetter.toUpperCase() + others.join("");
-  });
-  return capitalizedWords.join(" ");
+  return 
 };
 
 module.exports = transformFirstLetterToUppercase;
